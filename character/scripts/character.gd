eed000ad6115cbbda39958d672cf6c77d433c5de
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-10-23 17:23:39 -0300

    attack hitbox and damage to enemy added

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -14,6 +14,7 @@ var _is_attacking: bool = false
 @export var _animation_tree: AnimationTree = null
 
 func _ready() -> void:
+	_animation_tree.active = true
 	_state_machine = _animation_tree["parameters/playback"]
 
 func _physics_process(_delta: float) -> void:
@@ -66,12 +67,5 @@ func _on_attack_timer_timeout() -> void:
 	_is_attacking = false
 
 
-
-
-
-
-
-
-
-
-
+func _on_attack_area_body_entered(body):
+	pass # Replace with function body.
